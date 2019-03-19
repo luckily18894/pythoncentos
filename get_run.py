1991df2312359b631a7989a3292a42adf2648b32
luckily18894 <185686792@qq.com>
2019-03-20 01:33:44 +0800

    2019.3.19 创建一个数据库

---
+++ get_run.py
@@ -0,0 +1,10 @@
+
+import sqlite3
+
+
+conn = sqlite3.connect('configmd5.sqlite')
+cursor = conn.cursor()
+cursor.execute("create table config_md5 (ip varchar(40), config varchar(99999), md5 varchar(999))")
+
+
+
